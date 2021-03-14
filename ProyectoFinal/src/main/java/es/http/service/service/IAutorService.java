22aa688b0f49687e0d6f2eaaa8a4bf321b1c2243
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-03-14 19:52:33 +0100

    Interfaces Autor, Ejemplar, Editorial e Idioma y sus implementaciones.

--- ProyectoFinal/src/main/java/es/http/service/service/IAutorService.java
+++ ProyectoFinal/src/main/java/es/http/service/service/IAutorService.java
@@ -1,5 +1,22 @@
 package es.http.service.service;
 
+import java.util.List;
+
+import es.http.service.dto.Autor;
+
 public interface IAutorService {
 
+	//Metodos del CRUD
+		public List<Autor> listarAutor(); //Listar All 
+		
+		public Autor guardarAutor(Autor Autor);	//Guarda un AsignadoA CREATE
+		
+		public Autor AutorXID(int id); //Leer datos de un AsignadoA READ
+		
+		public Autor actualizarAutor(Autor Autor); //Actualiza datos del AsignadoA UPDATE
+		
+		public void eliminarAutor(int id);// Elimina el AsignadoA DELETE
+
+		
+	
 }
--- ProyectoFinal/src/main/java/es/http/service/service/IdiomaServiceImpl.java
+++ ProyectoFinal/src/main/java/es/http/service/service/IdiomaServiceImpl.java
@@ -1,5 +1,40 @@
 package es.http.service.service;
 
-public class IdiomaServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import es.http.service.dto.Idioma;
+import es.http.service.dao.IIdiomaDAO;
+
+//Implementamos los métodos del service.
+public class IdiomaServiceImpl implements IIdiomaService{
+	@Autowired
+	IIdiomaDAO IIdiomaDAO;
+	@Override
+	public List<Idioma> listarIdioma() {
+		return IIdiomaDAO.findAll();
+	}
+
+	@Override
+	public Idioma guardarIdioma(Idioma Idioma) {
+		return IIdiomaDAO.save(Idioma);
+	}
+
+	@Override
+	public Idioma IdiomaXID(int id) {
+		return IIdiomaDAO.findById(id).get();
+	}
+
+	@Override
+	public Idioma actualizarIdioma(Idioma Idioma) {
+		return IIdiomaDAO.save(Idioma);
+	}
+
+	@Override
+	public void eliminarIdioma(int id) {
+		IIdiomaDAO.deleteById(id);
+		
+	}
 
 }
