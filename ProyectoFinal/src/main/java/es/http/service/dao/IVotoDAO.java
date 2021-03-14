adb967bbf8ce4ac3faf87f199251f19c69631003
Marccudi <45258590+Marccudi@users.noreply.github.com>
2021-03-14 21:16:00 +0100

    Sede, Trabajador y Voto

---
+++ ProyectoFinal/src/main/java/es/http/service/dao/IVotoDAO.java
@@ -0,0 +1,9 @@
+package es.http.service.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.http.service.dto.Voto;
+
+public interface IVotoDAO  extends JpaRepository<Voto, Integer>{
+
+}
