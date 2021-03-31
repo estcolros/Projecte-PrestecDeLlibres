2504cce4deb59cd8e44112209799533766619fa6
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-03-31 20:20:19 +0200

    Pulido.

--- ProyectoFinal/src/main/java/es/http/service/service/IEjemplarService.java
+++ ProyectoFinal/src/main/java/es/http/service/service/IEjemplarService.java
@@ -9,12 +9,12 @@ public interface IEjemplarService {
 	// Metodos del CRUD
 	public List<Ejemplar> listarEjemplar(); // Listar All
 
-	public Ejemplar guardarEjemplar(Ejemplar Ejemplar); // Guarda un AsignadoA CREATE
+	public Ejemplar guardarEjemplar(Ejemplar Ejemplar); // Guarda un Ejemplar CREATE
 
-	public Ejemplar EjemplarXID(int id); // Leer datos de un AsignadoA READ
+	public Ejemplar EjemplarXID(int id); // Leer datos de un Ejemplar READ
 
-	public Ejemplar actualizarEjemplar(Ejemplar Ejemplar); // Actualiza datos del AsignadoA UPDATE
+	public Ejemplar actualizarEjemplar(Ejemplar Ejemplar); // Actualiza datos del Ejemplar UPDATE
 
-	public void eliminarEjemplar(int id);// Elimina el AsignadoA DELETE
+	public void eliminarEjemplar(int id);// Elimina el Ejemplar DELETE
 
 }
--- ProyectoFinal/src/main/java/es/http/service/service/ITrabajadorService.java
+++ ProyectoFinal/src/main/java/es/http/service/service/ITrabajadorService.java
@@ -9,15 +9,15 @@ public interface ITrabajadorService {
 	// Metodos del CRUD
 	public List<Trabajador> listarTrabajador(); // Listar All
 
-	public Trabajador guardarTrabajador(Trabajador Trabajador); // Guarda un AsignadoA CREATE
+	public Trabajador guardarTrabajador(Trabajador Trabajador); // Guarda un Trabajador CREATE
 
-	public Trabajador TrabajadorXID(int id); // Leer datos de un AsignadoA READ
+	public Trabajador TrabajadorXID(int id); // Leer datos de un Trabajador READ
 	
 	public Trabajador TrabajadorXuser(String user); //Leer datos de un trabajador por username
 	
 
-	public Trabajador actualizarTrabajador(Trabajador Trabajador); // Actualiza datos del AsignadoA UPDATE
+	public Trabajador actualizarTrabajador(Trabajador Trabajador); // Actualiza datos del Trabajador UPDATE
 
-	public void eliminarTrabajador(int id);// Elimina el AsignadoA DELETE
+	public void eliminarTrabajador(int id);// Elimina el Trabajador DELETE
 
 }
